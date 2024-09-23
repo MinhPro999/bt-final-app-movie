5e93318467fbb47f2ee755f347d1dfe1529d089e
huyho-vuatho <huy.vietho@vuatho.com>
2024-09-23 15:57:26 +0700

    Upload lesson 19's Assignment Implementation

--- lib/lesson_18_19_assignment/screens/movie_detail/movie_detail_screen.dart
+++ lib/lesson_18_19_assignment/screens/movie_detail/movie_detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_learning/lesson_18_assignment/widgets/about_content.dart';
-import 'package:flutter_learning/lesson_18_assignment/widgets/sessions_content.dart';
+import 'package:flutter_learning/lesson_18_19_assignment/screens/movie_detail/widgets/about_content.dart';
+import 'package:flutter_learning/lesson_18_19_assignment/screens/movie_detail/widgets/sessions_content.dart';
+import 'package:flutter_learning/lesson_18_19_assignment/theme/my_theme.dart';
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(fontFamily: "Inter"),
+      theme: MyTheme.darkTheme(),
       home: const MovieDetailScreen(),
     );
   }
@@ -32,44 +33,18 @@ class _MovieDetailScreenState extends State<MovieDetailScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xff1A2232),
       appBar: AppBar(
         leading: IconButton(
             onPressed: () {},
             icon: const Icon(
               Icons.arrow_back_ios,
-              color: Color(0xff637394),
             )),
         title: const Text(
           "The Batman",
-          style: TextStyle(
-              fontSize: 18,
-              color: Colors.white,
-              fontFamily: 'Inter',
-              fontWeight: FontWeight.w700),
         ),
-        backgroundColor: const Color(0xff1A2232),
         bottom: TabBar(
           controller: _tabController,
-          indicatorColor: const Color(0xffFF8036),
-          unselectedLabelStyle: const TextStyle(
-            color: Color(0xFF637394),
-            fontSize: 16,
-            fontWeight: FontWeight.w700,
-          ),
-          labelStyle: TextStyle(
-              color: const Color(0xffFF8036),
-              fontSize: 16,
-              fontWeight: FontWeight.w700,
-              shadows: [
-                Shadow(
-                  blurRadius: 16,
-                  color: const Color(0xffFF8036).withOpacity(.5),
-                )
-              ]),
-          indicatorSize: TabBarIndicatorSize.tab,
           indicatorWeight: 2,
-          dividerColor: Colors.transparent,
           tabs: const [
             Tab(
               text: "About",
