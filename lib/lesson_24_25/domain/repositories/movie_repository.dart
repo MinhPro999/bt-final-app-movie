750033651a9b757fd6255f6dc99ecfa7e0cc19a2
huyho-vuatho <huy.vietho@vuatho.com>
2024-10-11 04:25:46 +0700

    Finished Assignment

--- lib/lesson_24_25/domain/repositories/movie_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_learning/lesson_24_25/domain/entities/genre.dart';
-import 'package:flutter_learning/lesson_24_25/domain/entities/image_configuration.dart';
-import 'package:flutter_learning/lesson_24_25/domain/entities/movie.dart';
-
-abstract class MovieRepository {
-  Future<List<Genre>?> getGenre();
-  Future<ImageConfigInfo?> getImageInfo();
-  Future<List<Movie>?> getMovies();
-}
